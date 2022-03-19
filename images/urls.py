36cb944ab0422f1d4111b017364dcecb8c34d7dd
liyatijin <101854065+liyatijin@users.noreply.github.com>
2022-03-19 21:44:00 +0800

    Update urls.py

--- images/urls.py
+++ images/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('signup',views.signup,name='signup'),
     path('signin',views.signin,name='signin'),
     path('signout',views.signout,name='signout'),
-    path('addfile',views.addfile,name='addfile'),
 ]
